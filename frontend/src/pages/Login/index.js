c9934745a7bf93f9f31eddcc62d2a79bd2922b52
dornelasjonas <dornelasjonas96@hotmail.com>
2020-01-11 15:17:40 -0300

    Código inicial

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+    componentDidMount() {
+
+    }
+    render() {
+        return  (
+            <div>
+                Login teste
+            </div>
+        )
+    }
+}
